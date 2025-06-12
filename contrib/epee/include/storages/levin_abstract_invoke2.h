38d12e19afa525490751547adb6095b3f89d7006
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2025-06-12 19:05:48 -0400

    Update levin_abstract_invoke2.h

--- contrib/epee/include/storages/levin_abstract_invoke2.h
+++ contrib/epee/include/storages/levin_abstract_invoke2.h
@@ -27,6 +27,7 @@
 #pragma once
 
 #include <boost/bind/bind.hpp>
+using namespace boost::placeholders;
 #include "portable_storage_template_helper.h"
 #include <boost/utility/value_init.hpp>
 #include "net/levin_base.h"
