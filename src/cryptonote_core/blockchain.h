4f9590ecf841c66fa911438e72e80c55c2691fe7
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2025-06-12 18:17:28 -0400

    Update blockchain.h

--- src/cryptonote_core/blockchain.h
+++ src/cryptonote_core/blockchain.h
@@ -29,11 +29,7 @@
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
 #pragma once
-namespace boost {
-namespace serialization {
-    using library_version_type = unsigned int;
-} }
-
+#include <boost/serialization/library_version_type.hpp>
 #include <boost/asio/io_service.hpp>
 #include <boost/serialization/serialization.hpp>
 #define BOOST_SERIALIZATION_VERSION_HPP
