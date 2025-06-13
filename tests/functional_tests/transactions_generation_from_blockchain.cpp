bd6bd64cfa10361b5a0b383a28152f217a232a7a
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2025-06-12 20:49:53 -0400

    Update transactions_generation_from_blockchain.cpp

--- tests/functional_tests/transactions_generation_from_blockchain.cpp
+++ tests/functional_tests/transactions_generation_from_blockchain.cpp
@@ -27,6 +27,7 @@
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // 
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
+#include <boost/serialization/version.hpp>  // ✅ FORCE IT FIRST
 
 #include "include_base_utils.h"
 using namespace epee;
