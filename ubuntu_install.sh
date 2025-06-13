ac8402185a7808f3e0fa6243749f775db848ab01
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2025-06-12 21:30:22 -0400

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get update && \
-apt-get install --yes build-essential cmake pkg-config libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind-dev libpcsclite-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
+apt-get install --yes build-essential cmake pkg-config libboost-all-dev libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind-dev libpcsclite-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
 echo "Dependencies installation complete"
 echo "Download Submodules"
 
