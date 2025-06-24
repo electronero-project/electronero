831ea341982d91cad213ec9ae3276515ed6c165d
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2025-06-24 15:11:38 -0400

    Update cryptonote_protocol_handler_common.h

--- src/cryptonote_protocol/cryptonote_protocol_handler_common.h
+++ src/cryptonote_protocol/cryptonote_protocol_handler_common.h
@@ -45,7 +45,7 @@ namespace cryptonote
     virtual bool relay_transactions(NOTIFY_NEW_TRANSACTIONS::request& arg, cryptonote_connection_context& exclude_context)=0;
     //virtual bool request_objects(NOTIFY_REQUEST_GET_OBJECTS::request& arg, cryptonote_connection_context& context)=0;
     virtual void rescan_token_operations(uint64_t from_height) = 0;
-    virtual void process_token_tx(const cryptonote::transaction &tx) = 0;
+    virtual void process_token_tx(const cryptonote::transaction &tx, uint64_t height) = 0;
   };
 
   /************************************************************************/
@@ -62,7 +62,7 @@ namespace cryptonote
       return false;
     }
     virtual void rescan_token_operations(uint64_t) override {}
-    virtual void process_token_tx(const cryptonote::transaction &) override {}
+    virtual void process_token_tx(const cryptonote::transaction &, uint64_t) override {}
 
   };
 }
